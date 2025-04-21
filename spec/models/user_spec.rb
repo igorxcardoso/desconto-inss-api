d5d21dcfde71a325b452bd32a894377d34300c41
Igor Cardoso <iygorsilva@gmail.com>
2025-04-21 16:19:28 -0300

    feat: adição do faker e configuração do factorybot e teste o usando

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
 
+  it "cria um usuário válido com factory" do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+
   it "é inválido sem email" do
     user = User.new(password: "123456")
     expect(user).not_to be_valid
