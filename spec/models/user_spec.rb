11ba636fd0298829fdd263bce5759dd44ce41715
Igor Cardoso <iygorsilva@gmail.com>
2025-04-21 16:08:12 -0300

    feat: adição de teste usando rspec para model user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "é válido com email e senha" do
+    user = User.new(email: "teste@example.com", password: "123456")
+    expect(user).to be_valid
+  end
+
+  it "é inválido sem email" do
+    user = User.new(password: "123456")
+    expect(user).not_to be_valid
+  end
+end
